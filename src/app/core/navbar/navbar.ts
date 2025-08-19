f823e46b87dbdb5c7e6b8c414dea5c1cbcc80c6d
golfyassine <144442162+golfyassine@users.noreply.github.com>
2025-08-19 15:22:12 +0100

    end

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class Navbar {}
+export class Navbar {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return !!this.auth.getToken();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
